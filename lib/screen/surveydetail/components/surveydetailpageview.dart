52564470c701aabbaf307d86ed2646f165b9651f
Philipp Haslinger <philipp.haslinger@axonivy.com>
2020-06-17 19:56:30 +0200

    change from stateless to stateful widget to call dispose for pageview controller

--- lib/screen/surveydetail/components/surveydetailpageview.dart
+++ lib/screen/surveydetail/components/surveydetailpageview.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:xplatsurveydemo/model/surveyDetails.dart';
 import 'package:xplatsurveydemo/screen/question/questiontypedispatcher.dart';
 
-class SurveyDetailsPageView extends StatelessWidget {
+class SurveyDetailsPageView extends StatefulWidget {
   SurveyDetailsPageView({this.surveyDetail});
 
+  final SurveyDetail surveyDetail;
+
+  @override
+  _SurveyDetailsPageViewState createState() => _SurveyDetailsPageViewState();
+}
+
+class _SurveyDetailsPageViewState extends State<SurveyDetailsPageView> {
   final PageController controller = PageController();
 
-  final SurveyDetail surveyDetail;
   final Map<int, Color> colorMap = {
     0: Colors.red,
     1: Colors.orange,
@@ -17,12 +23,18 @@ class SurveyDetailsPageView extends StatelessWidget {
   };
 
   @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return PageView.builder(
         controller: controller,
-        itemCount: surveyDetail.questions.length,
+        itemCount: widget.surveyDetail.questions.length,
         itemBuilder: (context, index) {
-          return generateAnswerByType(surveyDetail: surveyDetail, index: index, controller: controller);
+          return generateAnswerByType(surveyDetail: widget.surveyDetail, index: index, controller: controller);
         });
   }
 }
